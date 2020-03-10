88206569391e39c9bde1fc6456549e08de10ffd9
github alison9797 <aliszak9727@gmail.com>
2020-03-10 15:49:18 -0400

    Google Search Test

--- src/test/java/com/automation/tests/day2/GoogleSearchTest.java
+++ src/test/java/com/automation/tests/day2/GoogleSearchTest.java
@@ -16,6 +16,9 @@ public class GoogleSearchTest {
         driver.get("http://google.com");
         Thread.sleep(2);
         //it will look for some element with the name "q"
+        //name is one of the Selenium locators
+        //we use locators to find webElements
+        //to choose locator, just use By.locator
         WebElement search = driver.findElement(By.name("q"));
         //once we found an element , how to enter text
         //to enter text use sendKeys() method
@@ -23,6 +26,12 @@ public class GoogleSearchTest {
         //use Key.ENTER - perform keyboard click
         search.sendKeys("Java", Keys.ENTER);
         Thread.sleep(2);
+        //Thread.sleep() - is used to pause java program
+        //if you see <a> element it's called link
+        ///visible text of this link , can be used by selenium to find this element
+        WebElement news = driver.findElement(By.linkText("News"));
+        news.click();//to click on the element
+        Thread.sleep(4);
 
 
         driver.quit();
