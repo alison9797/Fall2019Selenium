742e816d68537a2cb9498f0f6de21bf707fd1afe
github alison9797 <aliszak9727@gmail.com>
2020-03-10 14:39:47 -0400

    Created basic navigation class

--- src/test/java/com/automation/tests/day2/JumpToNewWindow.java
+++ src/test/java/com/automation/tests/day2/JumpToNewWindow.java
@@ -22,9 +22,42 @@ public class JumpToNewWindow {
 
         System.out.println(windowHandle);
         Set< String> windowsHandles = driver.getWindowHandles();
+//driver.getWindowHandles();   <--- returns id of all currently open windows
+        //Set <-- doesn't allow duplicates(REQUARED TO USE)
+        System.out.println(windowsHandles);
+
+        //since we have all windows and we have an ids for them
+        //we can say switch to something that is not equals to old windows
+        System.out.println("BEFORE SWITCH : " + driver.getCurrentUrl());
+
+        for (String windowId : windowsHandles ) {
+            if (!windowId.equals(windowHandle)){
+                driver.switchTo().window(windowId);
+            }
+
+        }
+
+        System.out.println("AFTER SWITCH : "+driver.getCurrentUrl());
 
 
+driver.close();
 
+    }
 
+    /***
+     * This method helps to switch in between windows based on page title
+     * @param pageTitle
+     * @param driver
+     */
+    public static void switchToWindowBasedOnTitle(String pageTitle, WebDriver driver){
+        Set<String > windows = driver.getWindowHandles();
+        for (String window : windows ) {
+
+            driver.switchTo().window(window);
+            if (driver.getTitle().equals(pageTitle)){
+                break;
+            }
+
+        }
     }
 }
--- src/test/java/com/automation/tests/day6/SelectByTextMultipleOptions.java
+++ src/test/java/com/automation/tests/day6/SelectByTextMultipleOptions.java
@@ -4,7 +4,10 @@ import com.automation.tests.utilities.BrowserUtilities;
 import com.automation.tests.utilities.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class SelectByTextMultipleOptions {
     public static void main(String[] args) {
@@ -21,6 +24,18 @@ public class SelectByTextMultipleOptions {
         languagesSelect.selectByVisibleText("JavaScript");
         languagesSelect.selectByVisibleText("Python");
 
+        //let's get all selected options
+        List<WebElement> selectedLanguages = languagesSelect.getAllSelectedOptions();
+
+
+        for (WebElement selectedLanguage :selectedLanguages){
+            System.out.println(selectedLanguage.getText());
+        }
+        BrowserUtilities.wait(3);
+
+        languagesSelect.deselectByVisibleText("Java");
+        BrowserUtilities.wait(3);
+        languagesSelect.deselectAll();
 
 
         BrowserUtilities.wait(3);
