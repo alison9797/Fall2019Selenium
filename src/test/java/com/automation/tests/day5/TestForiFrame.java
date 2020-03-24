81c512e9e6297009f48f59b7056c0c8f2a893fb1
github alison9797 <aliszak9727@gmail.com>
2020-03-24 17:17:18 -0400

    Nested Frames

--- src/test/java/com/automation/tests/day5/TestForiFrame.java
+++ src/test/java/com/automation/tests/day5/TestForiFrame.java
@@ -15,14 +15,25 @@ public class TestForiFrame {
         driver.get("http://practice.cybertekschool.com/iframe");
         BrowserUtilities.wait(4);
 //before looking for that element , we need to jump to that frame
+        //you can specify :name , id , index or webelement of the frame
+        //it's like we are jumping to another layer
         driver.switchTo().frame("mce_0_ifr");
         //now , this content will be visible
         WebElement textInput = driver.findElement(By.id("tinymce"));
 
         System.out.println(textInput.getText());
+        BrowserUtilities.wait(4);
+
+        textInput.clear();//to delete the text
+        textInput.sendKeys("Hello, World!");
+        BrowserUtilities.wait(4);
 
         //exit from the frame
         driver.switchTo().defaultContent();
+        WebElement heading = driver.findElement(By.tagName("h3"));
+        System.out.println(heading.getText());
+
+
         driver.quit();
     }
 }
