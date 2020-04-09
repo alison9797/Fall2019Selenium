3378097d39959db99b501ab484abe418e6f8b427
github alison9797 <aliszak9727@gmail.com>
2020-04-09 17:33:17 -0400

    SearchTest

--- src/test/java/com/automation/tests/day8/SearchTest.java
+++ src/test/java/com/automation/tests/day8/SearchTest.java
@@ -1,5 +1,6 @@
 package com.automation.tests.day8;
 
+
 import com.automation.tests.utilities.BrowserUtilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
@@ -7,7 +8,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -15,32 +15,26 @@ import org.testng.annotations.Test;
 import java.util.List;
 
 public class SearchTest {
-
     private WebDriver driver;
+
     @Test
     public void googleSearchTest(){
-        driver.get("http://google.com");
+        driver.get("https://www.google.com");
         driver.findElement(By.name("q")).sendKeys("java", Keys.ENTER);
         BrowserUtilities.wait(2);
-        //since every search item has a tag name <h3>
+
+        //since every search item has a tag name "h3"
         //it's the easiest way to collect all of them
         List<WebElement> searchItems = driver.findElements(By.tagName("h3"));
         for (WebElement searchItem : searchItems) {
-            String var = searchItem.getText();
-            //if there is a text - print it
-            if (!var.isEmpty()){
-                System.out.println(var);
-                //verify that every search result contains "java"
-                Assert.assertTrue(var.toLowerCase().contains("java"));
-            }
-
+            System.out.println(searchItem.getText());
         }
 
     }
 
     @BeforeMethod
-    public void setup(){
-        //setup webdriver
+    public void setDriver(){
+        //set up driver
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
 
@@ -49,7 +43,10 @@ public class SearchTest {
     public void teardown(){
         //close browser and destroy webdriver object
         driver.quit();
-
     }
+
+    
+
+
 
 }
