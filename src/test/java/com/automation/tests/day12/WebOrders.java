a356ba03a8ac280558117045cb6b7dccee656a58
github alison9797 <aliszak9727@gmail.com>
2020-04-23 16:37:13 -0400

    WebOrders

--- src/test/java/com/automation/tests/day12/WebOrders.java
+++ src/test/java/com/automation/tests/day12/WebOrders.java
@@ -1,4 +1,76 @@
 package com.automation.tests.day12;
 
+import com.automation.tests.utilities.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
 public class WebOrders {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeMethod
+    public void setup(){
+        driver = DriverFactory.createDriver("chrome");
+        wait = new WebDriverWait(driver, 10);
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/weborders");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test", Keys.ENTER);
+
+    }
+
+    @Test
+    public void checkBoxTest(){
+        driver.findElement(By.id("ctl00_MainContent_btnCheckAll")).click();
+
+    List<WebElement> checkBoxes =  driver.findElements(By.tagName("checkbox"));
+
+        for (WebElement checkBox : checkBoxes ) {
+            Assert.assertTrue(checkBox.isSelected());
+
+        }
+
+    }
+
+
+    /***
+     * Go to web orders
+     * Verify that Steve Johns zip code is 21233
+     * Then update his zip code to 20002
+     * Then verify that Steve Johns zip code is 20002
+     */
+
+    @Test
+    public void updateZipCode(){
+        WebElement zipcode = driver.findElement(By.xpath("//td[text()='Steve Johns']//following-sibling::td[7]"));
+        Assert.assertEquals(zipcode.getText(), "21233");
+
+        //click on update image
+        driver.findElement(By.xpath(" //td[text()='Steve Johns']//following-sibling::td/input")).click();
+
+        WebElement zipcodeInput = driver.findElement(By.id("ctl00_MainContent_fmwOrder_TextBox5"));
+        zipcodeInput.clear();
+        zipcodeInput.sendKeys("20002");
+
+        driver.findElement(By.id("ctl00_MainContent_fmwOrder_UpdateButton")).click();
+        //after reload the page , then old element is not valid anymore, so we need to find it again
+        zipcode = driver.findElement(By.xpath("//td[text()='Steve Johns']//following-sibling::td[7]"));
+        Assert.assertEquals(zipcode.getText(), "20002");
+
+
+    }
+
+    @AfterMethod
+    public void teardown(){
+
+    }
 }
