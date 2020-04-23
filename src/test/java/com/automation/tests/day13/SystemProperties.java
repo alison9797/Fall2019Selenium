5b14bf3c2245eecaf0c62e2420af31e2a84a6d4a
github alison9797 <aliszak9727@gmail.com>
2020-04-23 17:01:49 -0400

    configuration.properties

--- src/test/java/com/automation/tests/day13/SystemProperties.java
+++ src/test/java/com/automation/tests/day13/SystemProperties.java
@@ -8,6 +8,18 @@ public class SystemProperties {
     public void test(){
 
         System.out.println(System.getProperty("user.dir"));
+        System.out.println(System.getProperty("os.name"));
+        //flexible path to downloads folder
+        //System.getProperty("user.home")+"/Downloads"
+        System.out.println(System.getProperty("user.home"));
+
+
+        String pathToDownloads = System.getProperty("user.home") + "/Downloads";
+        System.out.println(pathToDownloads);
+
+
+        System.out.println(System.getProperty("os.arch"));
+        //https://docs.oracle.com/javase/tutorial/essential/environment/sysprop.html
     }
 
 
