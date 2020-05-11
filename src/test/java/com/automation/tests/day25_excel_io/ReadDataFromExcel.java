df326852e309aaf4a2945cb44bf730bff098b0da
*alison9797* <*aliszak9727@gmail.com*>
2020-05-10 23:48:49 -0400

    Read Data From Excel File : newLoginTest

--- src/test/java/com/automation/tests/day25_excel_io/ReadDataFromExcel.java
+++ src/test/java/com/automation/tests/day25_excel_io/ReadDataFromExcel.java
@@ -1,11 +1,98 @@
 package com.automation.tests.day25_excel_io;
 
+import com.automation.tests.utilities.ExcelUtil;
+import org.apache.poi.ss.usermodel.*;
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.util.Map;
+
 
 public class ReadDataFromExcel {
 
 
     @Test
-    public void readExcelFileTest(){}
+    //any kind of file operations provides exceptions
+    public void readExcelFileTest() throws Exception {
+        //we need to get a file as an object
+
+        File file = new File("VytrackTestUsers.xlsx");
+
+        //object that represents excel file
+        Workbook workbook = WorkbookFactory.create(file);
+        //to specify what spreadsheet(page ) in a file we need
+        Sheet workSheet = workbook.getSheet("QA1-short");
+        //get 1st row
+        Row firstRow = workSheet.getRow(0);
+        //get 1st cell
+        Cell firstCell = firstRow.getCell(0);
+        //get string value
+        String value = firstCell.getStringCellValue();
+
+        String secondCellValue = firstRow.getCell(1).getStringCellValue();
+
+        System.out.println(value);
+        System.out.println(secondCellValue);
+
+
+        int lastCell = firstRow.getLastCellNum();
+        System.out.println("##############################");
+        for (int i = 0; i <lastCell ; i++){
+            System.out.println(firstRow.getCell(i) + " | ");
+
+        }
+        //returns indexes
+        int numberOfRows = workSheet.getLastRowNum();
+        //actual count of rows
+        int numberOfRows2 = workSheet.getPhysicalNumberOfRows();
+        System.out.println("\nNumber of rows: " + numberOfRows);
+
+        System.out.println("\nActual count of rows: " + numberOfRows2);
+
+
+        System.out.println("####################");
+        //how to pring the whole spreadsheet
+
+        for (int row = 0; row < workSheet.getPhysicalNumberOfRows(); row++) {
+            for (int cell = 0; cell < workSheet.getRow(row).getLastCellNum(); cell++) {
+                String cellValue = workSheet.getRow(row).getCell(cell).getStringCellValue();
+                System.out.println(cellValue + " | ");
+            }
+            System.out.println();
+
+        }
+    }
+
+    @Test
+    public void excelUtilityTest(){
+
+        String path = "VytrackTestUsers.xlsx";
+        String spreadsheet = "QA1-all";
+        ExcelUtil excelUtil = new ExcelUtil(path, spreadsheet);
+        //                      shortcut for for each loop
+        // or can do this way         (p -> System.out.println(p))
+        //excelUtil.getDataList().forEach(System.out::println);
+
+
+        //alternative way
+        for (Map<String, String> record : excelUtil.getDataList() ){
+            System.out.println(record);
+
+        }
+
+    }
+
+    /**
+     * this method returns a;; column names as a List of String
+     */
+    @Test
+    public void getColumnNamesTest(){
+        String path = "VytrackTestUsers.xlsx";
+        String spreadsheet = "QA1-all";
+        ExcelUtil excelUtil = new ExcelUtil(path, spreadsheet);
+
+        System.out.println(excelUtil.getColumnsNames());
+
+    }
 
 }
--- src/test/java/com/automation/tests/vytrack/login/NewLoginTests.java
+++ src/test/java/com/automation/tests/vytrack/login/NewLoginTests.java
@@ -3,8 +3,10 @@ package com.automation.tests.vytrack.login;
 import com.automation.pages.LoginPage;
 import com.automation.tests.utilities.BrowserUtilities;
 import com.automation.tests.utilities.Driver;
+import com.automation.tests.utilities.ExcelUtil;
 import com.automation.tests.vytrack.AbstractTestBase;
 import org.testng.Assert;
+import org.testng.SkipException;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -70,5 +72,31 @@ public class NewLoginTests extends AbstractTestBase {
 
     }
 
+    @Test(dataProvider = "credentialsFromExcel")
+    public void loginTestWithExcel(String execute , String username , String password ,  String firstName, String lastName , String result){
+        test = report.createTest("Login test for username: " + username);
+
+        if (execute.equals("y")){
+            LoginPage loginPage = new LoginPage();
+            loginPage.login(username, password);
+            test.info("Login as: " + username);
+            test.info(String.format("First name: &s , Last name: %s , Username: %s ", firstName , lastName, username));
+            test.pass("Successfully logged in as" + username);
+
+        } else {
+            //to skip some test with TestNG
+            throw new SkipException("Test was skipped for user " + username);
+        }
+
+    }
+
+    @DataProvider
+public Object [][] credentialsFromExcel(){
+        String path = "VytrackTestUsers.xlsx";
+        String spreadsheet = "QA3-short";
+        ExcelUtil excelUtil = new ExcelUtil(path, spreadsheet);
+        return excelUtil.getDataArray();
+
+}
 
 }
